76ab90e2602dfc81c768a9e35b74e129a417f413
RCheeCoding <78927256+RCheeCoding@users.noreply.github.com>
2021-02-11 18:07:37 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+var song = prompt("What song would you like to find the lyrics for? (Do not include punctuation)").replaceAll(" ", "-")
+var creator = prompt("Who made that song")
+var url = "genius.com/" + creator + "-" + song
+fetch(url);
+.then(res => res.text())
+.then(text => {
+  console.log(text)
+})
